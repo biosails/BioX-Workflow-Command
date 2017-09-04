c2aa4ce0975cf41f569b731a6ae81e1e9008d3d0
Jillian Rowe <jillian.e.rowe@gmail.com>
2017-09-04 13:33:46 +0400

    finished feature

--- lib/BioX/Workflow/Command/run/Rules/Directives/Interpolate/Mustache.pm
+++ lib/BioX/Workflow/Command/run/Rules/Directives/Interpolate/Mustache.pm
@@ -5,4 +5,26 @@ use namespace::autoclean;
 
 use Template::Mustache;
 
+sub interpol_directive {
+    my $self   = shift;
+    my $source = shift;
+    my $text   = '';
+
+    #The $ is not always at the beginning
+    if ( exists $self->interpol_directive_cache->{$source} && $source !~ m/{/ )
+    {
+        return $self->interpol_directive_cache->{$source};
+    }
+
+    if ( $source !~ m/{/ ) {
+        $self->interpol_directive_cache->{$source} = $source;
+        return $source;
+    }
+
+    $text = Template::Mustache->render( $source, $self );
+    $self->interpol_directive_cache->{$source} = $text;
+    return $text;
+    
+}
+
 1;
